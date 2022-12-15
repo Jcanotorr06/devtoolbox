53e22d02605f34ff0d7ea60b60ad0d074f5050f0
Jcanotorr06 <jcano062001@gmail.com>
2022-12-14 21:50:59 -0500

    feat: create global store for email

---
+++ src/state/emailStore.ts
@@ -0,0 +1,21 @@
+import create from 'zustand';
+import { devtools } from 'zustand/middleware';
+import type { Mailbox } from '../types/mailbox';
+
+type EmailStore = {
+    email: Mailbox | null;
+    openEmail: (email: Mailbox) => void;
+    closeEmail: () => void;
+}
+
+const useEmailStore = create<EmailStore>()(
+    devtools(
+        (set) => ({
+            email: null,
+            openEmail: (email: Mailbox) => set(() => ({ email })),
+            closeEmail: () => set(() => ({ email: null })),
+        })
+    )
+)
+
+export default useEmailStore;
