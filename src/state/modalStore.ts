8544c6f2efe8d110e93dd199579089589cc6e9dc
Jcanotorr06 <jcano062001@gmail.com>
2022-12-14 21:50:49 -0500

    feat: create global store for modal

---
+++ src/state/modalStore.ts
@@ -0,0 +1,23 @@
+import create from 'zustand';
+import { devtools } from 'zustand/middleware';
+import type { ModalTypes } from '../components/Modals/ModalManager';
+
+type ModalStore = {
+    isModalOpen: boolean;
+    modalId: ModalTypes;
+    openModal: (id: ModalTypes) => void;
+    closeModal: () => void;
+}
+
+const useModalStore = create<ModalStore>()(
+    devtools(
+        (set) => ({
+            isModalOpen: false,
+            modalId: '',
+            openModal: (id: ModalTypes) => set(() => ({ isModalOpen: true, modalId: id })),
+            closeModal: () => set(() => ({ isModalOpen: false, modalId: '' })),
+        })
+    )
+)
+
+export default useModalStore;
