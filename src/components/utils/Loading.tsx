f3b6e86737f13f644269879b2cb28b9a89fbdf11
Jcanotorr06 <jcano062001@gmail.com>
2022-12-14 21:48:02 -0500

    feat: create loading overlay component

---
+++ src/components/utils/Loading.tsx
@@ -0,0 +1,20 @@
+import { type FC } from 'react'
+import { Oval } from 'react-loader-spinner'
+
+const Loading:FC = () => {
+  return (
+    <div className="bg-gray-300 bg-opacity-50 w-full h-full flex items-center justify-center">
+        <Oval
+            height={40}
+            width={40}
+            visible={true}
+            color="#4fa94d"
+            secondaryColor='#4fa94d'
+            strokeWidth={4}
+            strokeWidthSecondary={4}
+        />
+    </div>
+  )
+}
+
+export default Loading
