797adb6ece389e1d9a5638610cde6dd8bfd7b57f
Jcanotorr06 <jcano062001@gmail.com>
2022-12-14 21:49:20 -0500

    feat: create base layout
    Create base layout component and implement alert and modal managers

---
+++ src/components/Layouts/BaseLayout.tsx
@@ -0,0 +1,39 @@
+import { motion } from "framer-motion"
+import { type FC } from "react"
+import { useSidebarStore } from "../../state"
+import AlertManager from "../Alerts/AlertManager"
+import ModalManager from "../Modals/ModalManager"
+import Sidebar from "../Navigation/Sidebar"
+
+type Props = {
+    children: React.ReactNode
+}
+const BaseLayout:FC<Props> = ({children}:Props) => {
+
+  const { isSidebarOpen } = useSidebarStore()
+
+  const variants = {
+    open: { marginLeft: "16rem" },
+    closed: { marginLeft: "8rem" }
+  }
+
+  return (
+    <div className="flex w-screen h-screen relative bg-base-200">
+        <Sidebar />
+        <motion.section
+          animate={
+            isSidebarOpen ? "open" : "closed"
+          }
+          variants={variants}
+          initial={false} 
+          className={`flex flex-grow bg-base-200 z-10 h-full p-4`}
+        >
+            {children}
+        </motion.section>
+        <AlertManager/>
+        <ModalManager/>
+    </div>
+  )
+}
+
+export default BaseLayout
