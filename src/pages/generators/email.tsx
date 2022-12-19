1b291e48f23b4e60fddc492b32b123f63ba0736c
Jcanotorr06 <jcano062001@gmail.com>
2022-12-18 22:17:06 -0500

    feat: use tool title and description for page SEO

--- src/pages/generators/email.tsx
+++ src/pages/generators/email.tsx
@@ -2,14 +2,16 @@ import { type NextPage } from "next";
 import { NextSeo } from "next-seo";
 import { Fragment } from "react";
 import { MessageDetails, Address, Inbox } from "../../components/Email";
+import tools from "../../utils/tools";
 
 const Email:NextPage = () => {
+    const tool = tools.find(tool => tool.id === "email")
 
     return (
         <Fragment>
             <NextSeo
-                title="Temporary Email Generator | Dev Toolbox"
-                description="Genereate a disposable temporary email address."
+                title={`${tool?.name} | Dev Toolbox`}
+                description={tool?.description}
             />
             <main className="grid gap-4 grid-flow-row grid-cols-1 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-12 grid-rows-6 w-full">
                 <section className="col-span-5 row-span-1 flex flex-col gap-4 rounded-lg bg-base-100 p-4 shadow-md">
